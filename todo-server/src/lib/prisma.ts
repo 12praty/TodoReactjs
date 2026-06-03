f442a92582bae89f7750f5fafb20777f23dae153
Pratyush8678 <kumarpratyush574@gmail.com>
2026-06-03 13:04:29 +0530

    feat: initialize Prisma ORM with PostgreSQL adapter, define User, Todo, and RefreshToken models, and configure pnpm workspace

---
+++ todo-server/src/lib/prisma.ts
@@ -0,0 +1,11 @@
+// import process from "node:process"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "../../generated/prisma/client.js"
+
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({ adapter })
+export default prisma
