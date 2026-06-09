63501db559eae60edec7ee600b52a1c252ac03c1
Pratyush8678 <kumarpratyush574@gmail.com>
2026-06-09 20:19:55 +0530

    feat: implement JWT token generation and refresh token persistence utilities

---
+++ todo-server/src/utils/token.ts
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken"
+import prisma from "../lib/prisma.js"
+
+export const accessToken = ((userId:string):string=>{
+    return jwt.sign({userId},process.env.ACCESSTOKEN!,{expiresIn:'15m'})
+
+})
+
+export const refreshToken = ((userId:string):string=>{
+    return jwt.sign({userId},process.env.REFRESHTOKEN!,{expiresIn:'7d'})
+
+})
+
+
+
+export const saveRefreshToken = async (userId: string, token: string): Promise<void> => {
+    const expiredAt = new Date();
+    expiredAt.setDate(expiredAt.getDate() + 7);
+    await prisma.refreshToken.create({ data: { token, userId, expiredAt } });
+}
