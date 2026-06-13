c776d3427f521e0089e1130ba80bf1e45d49d615
Pratyush8678 <kumarpratyush574@gmail.com>
2026-06-13 12:20:02 +0530

    feat: implement login endpoint with JWT authentication and refresh token storage

--- todo-server/src/controller/auth.controller.ts
+++ todo-server/src/controller/auth.controller.ts
@@ -35,3 +35,23 @@ export const register = async (req: Request, res: Response) => {
         });
     }
 };
+export const login =async (req:Request,res:Response)=>{
+    try{
+        const{email,password}=req.body;
+        const user=await prisma.user.findUnique({where:{email}});
+        if(!user){
+            res.status(404).json({message:"User not exists"})
+        }
+        const valid=await argon2.verify(user?.password!,password);
+        if(!valid){
+            res.status(404).json({message:"User not exists"})
+        }
+        const accessToken=generateAccessToken(user?.id!)
+        const refreshToken=generateRefreshToken(user?.id!)
+        await saveRefreshToken(user?.id!,refreshToken)
+        return res.status(200).json({accessToken,refreshToken,user:{id:user?.id,email:user?.email}})
+
+    }catch(err){
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
